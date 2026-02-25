9ec0ddcb459a6f4f87dcd73d821115e7e388b912
Novak Urošević <novakurosevic@users.noreply.github.com>
2026-02-25 10:57:27 +0000

    Added pipeline and dockerization.

---
+++ pipeline/ingest_data.py
@@ -0,0 +1,76 @@
+import pandas as pd
+
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+dtype = {
+    "VendorID": "Int64",
+    "passenger_count": "Int64",
+    "trip_distance": "float64",
+    "RatecodeID": "Int64",
+    "store_and_fwd_flag": "string",
+    "PULocationID": "Int64",
+    "DOLocationID": "Int64",
+    "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+}
+
+parse_dates = [
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime"
+]
+
+def run():
+    pg_user = 'root'
+    pg_password = 'root'
+    pg_host = 'localhost'
+    pg_db = 'ny_taxi'
+    pg_port = 5432
+
+    target_table = 'yellow_taxi_data'
+
+    year = 2021
+    month = 1
+
+    chunksize = 100000
+
+    # Read a sample of the data
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+
+    df_iter = pd.read_csv(
+        prefix + f'yellow_tripdata_{year}-{month:02d}.csv.gz',
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
+    )
+
+    first = True
+
+    for df_chunk in tqdm(df_iter) :
+        if first:
+            df_chunk.head(0).to_sql(
+                name= target_table, 
+                con=engine,
+                if_exists='replace'
+            )
+
+            first = False
+        
+        df_chunk.to_sql(
+                name= target_table, 
+                con=engine,
+                if_exists='append'
+            )
+
+if __name__ == '__main__':
+    run()
